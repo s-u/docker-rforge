b65a326f1423337f634c8409df1fb53239902406
Simon Urbanek <simon.urbanek@R-project.org>
2021-08-29 01:26:04 -0500

    minor cleanup

--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,1 @@
-docker run -d --rm --name RForge -p 8080:80 -p 3690:3690 --mount type=bind,source=/www/rforge,target=/www/rforge --mount type=bind,source=/var/svnroot,target=/var/svnroot 709b5fd6261c
+docker run -d --rm --name RForge -p 127.0.0.1:8080:80 -p 3690:3690 --mount type=bind,source=/www/rforge,target=/www/rforge --mount type=bind,source=/var/svnroot,target=/var/svnroot rforge:v1
--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# docker run -p 8080:80 -p 3690:3690 --mount type=bind,source=/www/rforge,target=/www/rforge --mount type=bind,source=/var/svnroot,target=/var/svnroot -it
-
 /etc/init.d/apache2 start       # the legacy beast - main reason for doing this
 /etc/init.d/mysql start         # FIXME: migrate to sqlite so we don't need mysql
 /etc/init.d/openbsd-inetd start # for svnserve
